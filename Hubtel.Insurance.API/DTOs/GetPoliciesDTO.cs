727bab71cd03d01099dd2716d1547d03078be389
kkopoku <kk.opoku@outlook.com>
2025-03-12 10:03:55 +0000

    fix: added BSON Element to fix bug

--- Hubtel.Insurance.API/DTOs/GetPoliciesDTO.cs
+++ Hubtel.Insurance.API/DTOs/GetPoliciesDTO.cs
@@ -1,10 +1,15 @@
 namespace Hubtel.Insurance.API.DTOs;
 
 using Hubtel.Insurance.API.Models;
+using MongoDB.Bson.Serialization.Attributes;
 
 public class GetPoliciesDTO {
     public required string Id { get; set; }
+
+    [BsonElement("policyName")]
     public required string PolicyName { get; set; }
+
+    [BsonElement("policyId")]
     public int PolicyId { get; set; }
     public List<PolicyComponent> Components { get; set; } = [];
 }
